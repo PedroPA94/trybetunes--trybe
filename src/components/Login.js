7d1650804db0f94e77e4b784e8b04e12ad52d36c
Pedro Papini <ppapinia@gmail.com>
2022-07-01 16:14:01 -0300

    Resolve requisito 2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,68 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
+
+const MIN_NAME_LENGTH = 3;
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      loading: false,
+    };
+  }
+
+  toggleLoading = () => {
+    this.setState((prevState) => ({
+      loading: !prevState.loading,
+    }));
+  }
+
+  handleInput = ({ target }) => this.setState({ name: target.value })
+
+  validateUser = async ({ name }) => {
+    const { history } = this.props;
+    this.toggleLoading();
+    await createUser({ name });
+    history.push('/search');
+  }
+
   render() {
+    const { name, loading } = this.state;
+    const validName = name.length >= MIN_NAME_LENGTH;
+    if (loading) return <Loading />;
     return (
       <div data-testid="page-login">
-        <p>Ola</p>
+        <form>
+          <label htmlFor="name">
+            <input
+              type="text"
+              name="name"
+              onChange={ this.handleInput }
+              value={ name }
+              data-testid="login-name-input"
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ !validName }
+            onClick={ () => this.validateUser({ name }) }
+            data-testid="login-submit-button"
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
