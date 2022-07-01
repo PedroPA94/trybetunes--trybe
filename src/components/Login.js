56b299d7b2623fd4018e75c8443c6baf29499c81
Pedro Papini <ppapinia@gmail.com>
2022-07-01 15:44:34 -0300

    Resolve requisito 1

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        <p>Ola</p>
+      </div>
+    );
+  }
+}
+
+export default Login;
