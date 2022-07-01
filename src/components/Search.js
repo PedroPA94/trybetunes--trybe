d102977393aadbe2884249f01236c0d6f4fbfe03
Pedro Papini <ppapinia@gmail.com>
2022-07-01 17:55:00 -0300

    Resolve requisito 5

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,42 @@
 import React from 'react';
 
+const MIN_ARTIST_NAME_LENGTH = 2;
+
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      artistName: '',
+    };
+  }
+
+  handleInput = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      artistName: value,
+    });
+  }
+
   render() {
+    const { artistName } = this.state;
+    const validArtist = artistName.length >= MIN_ARTIST_NAME_LENGTH;
     return (
       <div data-testid="page-search">
-        <p>Search</p>
+        <input
+          type="text"
+          placeholder="Nome do artista"
+          value={ artistName }
+          onChange={ this.handleInput }
+          data-testid="search-artist-input"
+        />
+        <button
+          type="button"
+          onClick={ () => console.log('Olá!') }
+          disabled={ !validArtist }
+          data-testid="search-artist-button"
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
